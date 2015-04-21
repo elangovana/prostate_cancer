42579d9a9b4a9fc5c69a1cdb84e2c8bfa7a19857
elana <aparnaelangovan@aparnas-air.bigpond>
2015-04-21 20:42:05 +1000

    Split into train and test

---
+++ alg_random_forest.R
@@ -0,0 +1,5 @@
+alg_random_forest <- function(train_ct, train_lv, train_lm, train_mh, train_pm, train_vs,
+                              test_ct, test_lv, test_lm, test_mh, test_pm, test_vs){
+  library("randomForest")
+  
+}
--- main.R
+++ main.R
@@ -1,7 +1,8 @@
 ##########
 input_data_dir = "./input_dat"
 input_data_train_dir = file.path(input_data_dir, "training")
-
+count = 2
+test_count = count + 3
 
 ####Download data#############
 
@@ -24,3 +25,23 @@ VitalSign_synapse_entity <- file.path(input_data_train_dir,"VitalSign_training.c
 VitalSign_training <- read.csv(VitalSign_synapse_entity, header=T, na.strings=".")
 
 ##
+
+## Split training data into training and test
+train_ct <- CoreTable_training[c(1:count), ]
+train_lv <- LabValue_training[LabValue_training$RPT %in% train_ct$RPT, ]
+train_lm <- LesionMeasure_training[LesionMeasure_training$RPT %in% train_ct$RPT, ]
+train_mh <- MedHistory_training[MedHistory_training$RPT %in% train_ct$RPT, ]
+train_pm <- PriorMed_training [PriorMed_training $RPT %in% train_ct$RPT, ]
+train_vs <- VitalSign_training [VitalSign_training$RPT %in% train_ct$RPT, ]
+
+
+test_ct <- CoreTable_training[c(count+1:test_count), ]
+test_lv <- LabValue_training[LabValue_training$RPT %in% test_ct$RPT, ]
+test_lm <- LesionMeasure_training[LesionMeasure_training$RPT %in% test_ct$RPT, ]
+test_mh <- MedHistory_training[MedHistory_training$RPT %in% test_ct$RPT, ]
+test_pm <- PriorMed_training [PriorMed_training $RPT %in% test_ct$RPT, ]
+test_vs <- VitalSign_training [VitalSign_training$RPT %in% test_ct$RPT, ]
+
+source("./alg_random_forest.R")
+alg_random_forest(train_ct, train_lv, train_lm, train_mh, train_pm, train_vs,
+test_ct, test_lv, test_lm, test_mh, test_pm, test_vs)
